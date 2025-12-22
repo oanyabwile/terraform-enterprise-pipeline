1729d0de6d9280e3bfa7fcda34c5bc1c1efc7889
Omari Anyabwile <74150325+oanyabwile@users.noreply.github.com>
2025-12-22 05:22:46 +0000

    feat: add GitHub Actions IAM role with repo and branch trust

---
+++ infra/oidc/github-actions-role.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-terraform"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:oanyabwile/terraform-enterprise-pipeline:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
